89c8208e5af1b2647ee5d30a30da7c7377ecae9a
tybo-dev <tobias@diez.guru git config --global user.name tgit config --global user.email tobias@diez.guru>
2022-11-29 16:24:52 +0100

    further updates along the vids

--- src/BlogList.js
+++ src/BlogList.js
@@ -3,6 +3,7 @@ const BlogList = ({blogs, title}) => {
 //     const title = props.title;
 //   console.log(props, blogs);
 
+
   return (
     <div className="blog-list">
         <h2>{title}</h2>
@@ -11,6 +12,7 @@ const BlogList = ({blogs, title}) => {
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
+        
         </div>
       ))}
     </div>
--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,37 @@
 // import React from 'react';
-import { useState } from "react"; //importing the hook use state directly, no need for React..... in code
+import { useState, useEffect } from "react"; //importing the hook use state directly, no need for React..... in code
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "My new website", body: "lorem ipsum...", author: "Paz", id: 1 },
-    { title: "Welcome party!", body: "lorem ipsum...", author: "Ida", id: 2 },
-    { title: "Web dev top tips", body: "lorem ipsum...", author: "Paz", id: 3 },
-  ]);
+  const [blogs, setBlogs] = useState(null);
+
+
+  //this function will always be fired off once the page is rendered, first load and every time as the data changes
+  useEffect(() => {
+    //changing the state inside side effect could be a bad idea because you could create an infinite loop
+     console.log('use effect ran');
+     fetch('http://localhost:8000/blogs')
+      .then(res => {              //async is not possible inside useEffect, maybe with reference to external function
+          return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setBlogs(data);
+      }) 
+     
+
+     //dependency array: will only be run when certain conditions are met and not always at every re-render
+  }, []); //watches after variable in [] brackets, once it changes it will run the function useEffect
+ 
 
   return (
     <div className="home">
-        <BlogList blogs={blogs} title="All Blogs!"/>
+        {/* handle delete will be evoked inside BlogList.js component but it is defined here above*/}
+        {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
+     
+
+        {/* filter: return array for true items of call backfunction */}
+        {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'Paz')} title="Paz' Blogs!"/>  */}
     </div>
   )
 };
