5935c03cc7a60c0eefebd50f0fb8aa18acf89b12
tybo-dev <tobias@diez.guru>
2022-12-12 11:51:05 +0100

    home updated

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,11 @@ import BlogList from "./BlogList";
 import useFetch from "./useFetch";
 
 const Home = () => {
-  
+  const {
+    data: blogs,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/blogs");
 
   return (
     <div className="home">
