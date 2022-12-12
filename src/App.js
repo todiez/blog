303978549eccc7815c5b76d86df7d9b6de4224e5
tybo-dev <tobias@diez.guru>
2022-12-12 16:08:16 +0100

    router added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-
-import Navbar from './Navbar';
-import Home from './Home';
+import Navbar from "./Navbar";
+import Home from "./Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
-    return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Home />
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
