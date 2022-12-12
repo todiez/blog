62824bf036cfc345392e0564503b1b33e7cbca08
tybo-dev <tobias@diez.guru>
2022-12-12 11:46:48 +0100

    fetch component added

--- src/Home.js
+++ src/Home.js
@@ -1,37 +1,10 @@
 // import React from 'react';
 import { useState, useEffect } from "react"; //importing the hook use state directly, no need for React..... in code
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(null);
-
-  //this function will always be fired off once the page is rendered, first load and every time as the data changes
-  useEffect(() => {
-    //changing the state inside side effect could be a bad idea because you could create an infinite loop
-    setTimeout(() => {
-      fetch("http://localhost:8000/blogs") // start server command, cd into blog then npx json-server --watch data/database.json --port 8000
-        .then((res) => {
-          //.then necessary because async is not possible inside useEffect, maybe with reference to external function
-          console.log(res);
-          if (!res.ok) {
-            throw Error("Could not fetch the data for that resource");
-          }
-          return res.json();
-        })
-        .then((data) => {
-          setBlogs(data);
-          setIsPending(false);
-          setError(null);
-        })
-        .catch((err) => {
-          setIsPending(false);
-          setError(err.message);
-        });
-    }, 300);
-    //dependency array: will only be run when certain conditions are met and not always at every re-render
-  }, []); //watches after variable in [] brackets, once it changes it will run the function useEffect
+  
 
   return (
     <div className="home">
---
+++ src/useFetch.js
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    //changing the state inside side effect could be a bad idea because you could create an infinite loop
+    setTimeout(() => {
+      fetch(url) // start server command, cd into blog then npx json-server --watch data/database.json --port 8000
+        .then((res) => {
+          //.then necessary because async is not possible inside useEffect, maybe with reference to external function
+          console.log(res);
+          if (!res.ok) {
+            throw Error("Could not fetch the data for that resource");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setData(data);
+          setIsPending(false);
+          setError(null);
+        })
+        .catch((err) => {
+          setIsPending(false);
+          setError(err.message);
+        });
+    }, 300);
+    //dependency array: will only be run when certain conditions are met and not always at every re-render
+  }, []); //watches after variable in [] brackets, once it changes it will run the function useEffect
+
+  return {
+    data,
+    isPending,
+    error,
+  };
+};
+
+export default useFetch;
