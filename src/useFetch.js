02a2b928571155986a30c248a998c7345cf9a129
tybo-dev <tobias@diez.guru>
2022-12-13 14:02:46 +0100

    route detail added

--- src/useFetch.js
+++ src/useFetch.js
@@ -7,8 +7,10 @@ const useFetch = (url) => {
 
   useEffect(() => {
     //changing the state inside side effect could be a bad idea because you could create an infinite loop
+    const abortContr = new AbortController();
+
     setTimeout(() => {
-      fetch(url) // start server command, cd into blog then npx json-server --watch data/database.json --port 8000
+      fetch(url, { signal: abortContr.signal }) // start server command, cd into blog then npx json-server --watch data/database.json --port 8000
         .then((res) => {
           //.then necessary because async is not possible inside useEffect, maybe with reference to external function
           console.log(res);
@@ -23,12 +25,19 @@ const useFetch = (url) => {
           setError(null);
         })
         .catch((err) => {
-          setIsPending(false);
-          setError(err.message);
+          if (err.name === "AbortError") {
+            console.log("fetch aborted");
+          } else {
+            setIsPending(false);
+            setError(err.message);
+          }
         });
-    }, 3000);
+    }, 300);
+
+    return () => abortContr.abort();
+
     //dependency array: will only be run when certain conditions are met and not always at every re-render
-  }, []); //watches after variable in [] brackets, once it changes it will run the function useEffect
+  }, [url]); //watches after variable in [] brackets, once it changes it will run the function useEffect
 
   return {
     data,
