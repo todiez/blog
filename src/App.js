d4df17b11546c2777f8ad70585b64f911b0103c3
tybo-dev <tobias@diez.guru>
2022-12-13 22:16:58 +0100

    finalized

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./Home";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Create from "./Create";
 import BlogDetails from "./BlogDetails";
+import NotFound from "./NotFound";
 
 function App() {
   return (
@@ -18,7 +19,10 @@ function App() {
               <Create />
             </Route>
             <Route path="/blogs/:id">
-              <BlogDetails/>
+              <BlogDetails />
+            </Route>
+            <Route path="*">
+              <NotFound />
             </Route>
           </Switch>
         </div>
--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
@@ -8,10 +8,13 @@ const BlogDetails = () => {
     error,
     isPending,
   } = useFetch("http://localhost:8000/blogs/" + id);
+  const history = useHistory();
 
   const handleClick = () => {
     fetch('http://localhost:8000/blogs/' + blog.id, {
         method: 'DELETE'
+    }).then(() => {
+        history.push('/');
     })
   }
 
