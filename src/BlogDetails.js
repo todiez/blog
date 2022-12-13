3c6c283829bc6268cf41e9db8f72f236facac022
tybo-dev <tobias@diez.guru>
2022-12-13 14:03:06 +0100

    route detail added

---
+++ src/BlogDetails.js
@@ -0,0 +1,13 @@
+import { useParams } from "react-router-dom";
+
+const BlogDetails = () => {
+    const {} = useParams;
+
+    return (  
+        <div className="blog-details">
+            <h2>Blog-Details</h2>
+        </div>
+    );
+}
+ 
+export default BlogDetails;
