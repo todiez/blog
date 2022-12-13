38c6bf06ca0134e49bdca272b001370aa0e20309
tybo-dev <tobias@diez.guru>
2022-12-13 17:00:58 +0100

    input form added

--- src/Create.js
+++ src/Create.js
@@ -1,9 +1,45 @@
+import { useState } from "react";
+
 const Create = () => {
-    return ( 
-        <div className="create">
-            <h2>Add a New Blog</h2>
-        </div>
-     );
-}
- 
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("Paz");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = {title, body, author};
+        
+    }
+
+  return (
+    <div className="create">
+      <h2>Add a New Blog</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Blog Title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)} //makes changes in the input field possible
+        />
+        <label>Blog Body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)} //makes changes in the input field possible
+        ></textarea>
+        <label>Blog Author:</label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="Paz">Paz</option>
+          <option value="Ida">Ida</option>
+        </select>
+        <button>Add Blog</button>
+        <p>{title}</p>
+        <p>{body}</p>
+        <p>{author}</p>
+      </form>
+    </div>
+  );
+};
+
 export default Create;
